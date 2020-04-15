8b192168d1b7e2ba0e955aeb9743797bac57484a
angrynajam <najamiftikhar2020@gmail.com>
2020-04-15 11:07:14 +0500

    Models completed

---
+++ models/comment.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  text: String,
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'comment',
+    },
+    username: String,
+  },
+  campground: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'campground',
+    },
+    name: String,
+  },
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
